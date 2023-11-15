30f027422272e592a214312b96212c97e5edcabf
abigailvolk <114170078+abigailvolk@users.noreply.github.com>
2023-11-15 13:11:37 -0700

    wrangling function works with graphing function. Committing before implementing a new version of the graphing function

--- 01.R
+++ 01.R
@@ -87,18 +87,6 @@ test("y", "z")
 test("x", "z")
 
 
-#### Lookup table function ####
-
-get_value <- function(mykey, mylookupvector){
-  myvalue <- mylookupvector[mykey]
-  unname(myvalue)
-}
-
-test_df <- c("Q05" = "5th Percentile", "Q95" = "95th Percentile")
-
-
-
-
 #### Graph Prototype ####
 annual_rcp_list$Hist %>% ggplot(aes(x=date, y = mean)) + 
     geom_line(aes(y=Q05, color="5th Percentile", lty="5th Percentile"), lwd=1) +
@@ -131,52 +119,13 @@ annual_rcp_list$Hist %>% ggplot(aes(x=date, y = mean)) +
         legend.text = element_text(size=10),
         legend.title.align=0.5)
 
-graph_timeseries_quantile <- function(ts_list,
-                                      rcp,
-                                      ylow = Q05,
-                                      yhigh = Q95,
-                                      ylow_char = "5th Percentile",
-                                      yhigh_char = "95th Percentile",
-                                      hist_name = "Annual Mean Historical",
-                                      proj_name = "Projected Ensemble Annual Mean",
-                                      hist_rcp_name = "Hist",
-                                      xaxis = date, 
-                                      ysmooth = mean) {
-  df_hist <- ts_list[[hist_rcp_name]]
-  df_rcp <- ts_list[[rcp]]
-  df_hist %>% ggplot(aes(x={{xaxis}}, y = {{ysmooth}})) +
-    geom_line(aes(y={{ylow}}, color=ylow_char, lty=ylow_char), lwd=1) +
-    geom_line(aes(y={{yhigh}}, color=yhigh_char, lty=yhigh_char), lwd=1) +
-    geom_ribbon(aes(x={{xaxis}}, ymin = {{ylow}}, ymax = {{yhigh}}), fill = "#E0EEEE", alpha = 0.5) +
-    geom_smooth(method = "loess", se=F, col="gray")+
-    geom_line(aes(color = hist_name, lty = hist_name), lwd=1) +
-    # RCP lines and ribbon
-    geom_ribbon(data = df_rcp, aes(x={{xaxis}}, ymin = {{ylow}}, ymax = {{yhigh}}), fill = "#E0EEEE", alpha = 0.5) +
-    geom_smooth(data = df_rcp, aes(x={{xaxis}}, y = {{ysmooth}}), method = "loess", se=F, col="gray") +
-    geom_line(data = df_rcp, aes(x={{xaxis}}, y = mean, color = proj_name, lty = proj_name), lwd = 1)+
-    geom_line(data = df_rcp, aes(y={{ylow}}, color=ylow_char, lty=ylow_char), lwd=1) +
-    geom_line(data = df_rcp, aes(y={{yhigh}}, color=yhigh_char, lty=yhigh_char), lwd=1) +
-    theme_bw() +
-    scale_color_manual(name = "Legend", 
-                       values = c(ylow_char = "dodgerblue4", 
-                                  yhigh_char = "aquamarine",
-                                  proj_name = "red",
-                                  hist_name = "black")) +
-    scale_linetype_manual(name = "Legend",
-                          values = c(ylow_char = 3,
-                                     yhigh_char = 3,
-                                     proj_name = 1,
-                                     hist_name = 1)) + 
-    labs(x="Year", y="Annual Flow (cfs)", title=paste0("Annual Historical and RCP4.5 Projected Streamflow for WBC")) +
-    theme(plot.title = element_text(hjust=0.5, face="bold"),
-          axis.text=element_text(size=12),
-          axis.title=element_text(size=14,face="bold"),
-          legend.title = element_text(size=14),
-          legend.text = element_text(size=10),
-          legend.title.align=0.5)
-}
-graph_timeseries_quantile(annual_rcp_list, rcp = "45")
 
+
+
+
+#### Function Prototypes -- Not quite working how I want ####
+
+# Below function works, but is not customizable for different quantiles.
 graph_timeseries_quantile <- function(ts_list,
                                       rcp,
                                       ylow = Q05,
@@ -218,6 +167,7 @@ graph_timeseries_quantile <- function(ts_list,
           legend.title.align=0.5)
 }
 graph_timeseries_quantile(annual_rcp_list, rcp = "45")
+graph_timeseries_quantile(Annual_test, rcp = "45", xaxis = yr)
 
 
 
@@ -283,64 +233,5 @@ ggplot(data = madf2, aes(x=yr, y = value, color=model)) + geom_line()+#scale_col
   nps_theme2()#;plot+
 plot
 
-
-
-
-
-
-#### Inspiration ####
-# source : https://www.r-bloggers.com/2017/07/tidy-time-series-analysis-part-1/
-
-#### Figure inspiration, written by David Thoma
-
-##################ensemble data frames###########################
-ens_daily <- compiled %>%
-  group_by(rcp, date) %>%
-  dplyr::summarize(modrunoff = mean(modrunoff, na.rm = TRUE),quick = mean(quick, na.rm = TRUE),
-                   slow = mean(slow, na.rm = TRUE),total = mean(total, na.rm = TRUE))
-ens_daily<-as.data.frame(ens_daily);head(ens_daily)
-
-ens_monthly <- monthly_df %>%
-  group_by(rcp, yr_mo) %>%
-  dplyr::summarize(modrunoff = mean(modrunoff, na.rm = TRUE),quick = mean(quick, na.rm = TRUE),
-                   slow = mean(slow, na.rm = TRUE),total = mean(total, na.rm = TRUE))
-ens_monthly<-as.data.frame(ens_monthly);head(ens_monthly)
-
-ens_annual <- annual_df %>%
-  group_by(rcp, yr) %>%
-  dplyr::summarize(modrunoff = mean(modrunoff, na.rm = TRUE),quick = mean(quick, na.rm = TRUE),
-                   slow = mean(slow, na.rm = TRUE),total = mean(total, na.rm = TRUE))
-ens_annual<-as.data.frame(ens_annual);head(ens_annual)
-
-ens_annual_45<-subset(ens_annual, rcp == 45)
-ens_annual_45$yr<-as.numeric(ens_annual_45$yr)#http://127.0.0.1:25719/graphics/plot_zoom_png?width=1920&height=1137
-head(ens_annual_45); str(ens_annual_45)#http://127.0.0.1:25719/graphics/plot_zoom_png?width=1920&height=1137
-
-ens_annual_85<-subset(ens_annual, rcp == 85)
-ens_annual_85$yr<-as.numeric(ens_annual_85$yr)
-head(ens_annual_85); str(ens_annual_85)#http://127.0.0.1:8693/graphics/plot_zoom_png?width=1920&height=1137
-###########################################################################
-#plotting
-###########################################################################
-head(annual_df);tail(annual_df)
-madf<-melt(annual_df, id.vars=c("model", "yr", "rcp"), measure.vars=c("total"));head(madf)
-madf_sub<-subset(madf, model == c("Historical","BNU-ESM_rcp45"));head(madf_sub)#for plotting just one future with historical
-madf_sub<-subset(madf, model == c("Historical"));head(madf_sub)#for plotting just one future with historical
-#remove historical from futures for plotting and color coding separately
-madf2<-subset(madf, model !="Historical");unique(madf2$model)
-hist_sub<-subset(madf, model =="Historical")#pull historical data for plotting separately if needed
-
-plot<-ggplot(data = madf2, aes(x=yr, y = value, color=model)) + geom_line()+#scale_color_viridis(discrete=TRUE)+#scale_fill_gradientn(colours=c("black","gray"))+#, alpha=westus_3_hs
-  geom_line(data = ens_annual_85, aes(x=yr, y = total), col="red", size = 2)+#facet_wrap(~model)+ ylab("Stream flow (mm)") + xlab("Year")+
-  geom_line(data = ens_annual_45, aes(x=yr, y = total), col="blue", size = 2)+
-  geom_line(data = madf_sub, aes(x=yr, y=value), col = "black", size = 2)+
-  #facet_wrap()
-  xlab("Year")+ ylab("Annual flow (mm)")+
-  ggtitle("Wet Beaver Creek annual flow")+
-  #color=NAor lwd = 0.01
-  nps_theme2()#;plot+
-plot
-
-plot(ens_annual_45$yr, ens_annual_45$total, type = "l")
 
 
